af0d9a3e18b0bed1706c099a1777ee73f0bd288a
Viraj Raiyani <virajraiyani162@gmail.com>
2024-09-19 01:05:01 +0530

    Learned business logic

--- natours/app.js
+++ natours/app.js
@@ -7,22 +7,17 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-})
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
